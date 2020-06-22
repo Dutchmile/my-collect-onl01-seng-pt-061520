26fe134973e12126d811faa844f769423943299f
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 21:27:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,13 @@ def my_collect(collection)
   x = 0
   new_collection = []
   while x < collection.length
-    yield(collection[x])
+    new_collection << yield(collection[x])
     x = x + 1
   end
 new_collection
 end
 
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(collection) { |name| new_collection << name.split(" ").first}
+my_collect(collection) do |name|
+  name.split(" ").first
+end
