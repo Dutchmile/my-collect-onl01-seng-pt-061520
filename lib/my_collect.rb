be27f52a7633ce03c112b13363aee859036138a2
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 21:21:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  x = 0
+  new_collection = []
+  while x < collection.length
+    yield(collection[x])
+    x = x + 1
+  end
+new_collection
+end
+  
+my_collect(collection) do { |name| new_collection << name.split(" ").first}
